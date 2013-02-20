85e5f9653b5ef09bd472f6e91fa71ae83059d2cb
Alex Weber <alexweber15@gmail.com>
2013-02-20 20:23:58 -0300

    implemented taz_env notifications

---
+++ modules/custom/taz_env/taz_env.js
@@ -0,0 +1,17 @@
+(function($, Drupal){
+  // Use strict mode to reduce development errors.
+  // @link http://www.nczonline.net/blog/2012/03/13/its-time-to-start-using-javascript-strict-mode/
+  "use strict";
+
+  Drupal.behaviors.taz_env_notify = {
+    attach: function(context, settings) {
+      var $button = $('#taz-env-notify-close');
+
+      if ($button.length) {
+        $button.click(function() {
+          $(this).parent('#taz-env-notify').slideUp('fast');
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
---
+++ modules/custom/taz_env/taz_env_notify.tpl.php
@@ -0,0 +1,4 @@
+<div id="taz-env-notify" class="<?php echo $env; ?>">
+  <a href="javascript:;" id="taz-env-notify-close"><?php echo t('Close'); ?></a>
+  <div id="taz-env-notify-msg"><?php echo $env_message; ?></div>
+</div>
