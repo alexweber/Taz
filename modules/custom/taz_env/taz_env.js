6970150139429fc8a647fef81bfafa5dbad40b9a
Alex Weber <alexweber15@gmail.com>
2013-04-08 11:54:49 -0300

    remove strict mode from taz_env.js
    unfortunately it seems to be a global setting and causes problems with some contribs

--- modules/custom/taz_env/taz_env.js
+++ modules/custom/taz_env/taz_env.js
@@ -1,8 +1,4 @@
 (function($, Drupal){
-  // Use strict mode to reduce development errors.
-  // @link http://www.nczonline.net/blog/2012/03/13/its-time-to-start-using-javascript-strict-mode/
-  "use strict";
-
   Drupal.behaviors.taz_env_banner = {
     attach: function(context, settings) {
       var $button = $('#taz-env-notify-close');
